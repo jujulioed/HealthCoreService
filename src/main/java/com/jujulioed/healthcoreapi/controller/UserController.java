6293a6a9b46bb2516a7fb2e602179b350c61f2d5
jujulioed <julioeduardon9@hotmail.com>
2026-02-13 21:08:30 -0300

    Improve User controller and implement DockerFile and structure for running on containers
    Signed-off-by: jujulioed <julioeduardon9@hotmail.com>

--- src/main/java/com/jujulioed/healthcoreapi/controller/UserController.java
+++ src/main/java/com/jujulioed/healthcoreapi/controller/UserController.java
@@ -1,16 +1,11 @@
 package com.jujulioed.healthcoreapi.controller;
 
-
 import com.jujulioed.healthcoreapi.dto.UserInfoDTO;
 import com.jujulioed.healthcoreapi.entity.AuthRequest;
-import com.jujulioed.healthcoreapi.entity.UserInfo;
 import com.jujulioed.healthcoreapi.service.JwtService;
 import com.jujulioed.healthcoreapi.service.UserInfoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -29,14 +24,7 @@ public class UserController {
 
     @PostMapping("/generateToken")
     public String authenticateAndGetToken(@RequestBody AuthRequest authRequest) {
-        Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(authRequest.getEmail(), authRequest.getPassword())
-        );
-        if (authentication.isAuthenticated()) {
-            return jwtService.generateToken(authRequest.getEmail());
-        } else {
-            throw new UsernameNotFoundException("Invalid user request!");
-        }
+        return service.authenticateAndGetToken(authenticationManager, jwtService, authRequest);
     }
 
     @GetMapping("/user/userdata")
--- src/main/java/com/jujulioed/healthcoreapi/service/UserInfoService.java
+++ src/main/java/com/jujulioed/healthcoreapi/service/UserInfoService.java
@@ -2,9 +2,13 @@ package com.jujulioed.healthcoreapi.service;
 
 
 import com.jujulioed.healthcoreapi.dto.UserInfoDTO;
+import com.jujulioed.healthcoreapi.entity.AuthRequest;
 import com.jujulioed.healthcoreapi.entity.UserInfo;
 import com.jujulioed.healthcoreapi.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
@@ -49,10 +53,20 @@ public class UserInfoService implements UserDetailsService {
     // Add any additional methods for registering or managing users
     public String addUser(UserInfoDTO userInfoDTO) {
         // Encrypt password before saving
-        System.out.println(userInfoDTO);
         UserInfo userInfo = new UserInfo(userInfoDTO.name(), userInfoDTO.email(), userInfoDTO.roles(), userInfoDTO.password());
         userInfo.setPassword(encoder.encode(userInfo.getPassword()));
         repository.save(userInfo);
         return "User added successfully!";
+    }
+
+    public String authenticateAndGetToken(AuthenticationManager authenticationManager, JwtService jwtService, AuthRequest authRequest) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(authRequest.getEmail(), authRequest.getPassword())
+        );
+        if (authentication.isAuthenticated()) {
+            return jwtService.generateToken(authRequest.getEmail());
+        } else {
+            throw new UsernameNotFoundException("Invalid user request!");
+        }
     }
 }
